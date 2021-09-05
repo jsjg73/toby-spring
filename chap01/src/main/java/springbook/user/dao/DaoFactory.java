a5c8de5eb9e5563f2fe5168b305733fd2dcd0443
jsjg73 <kjs930703@naver.com>
2021-09-06 00:15:31 +0900

    1.8.3 DataSource 인터페이스로 변환 [2]
    자바 코드 설정 방식으로
    DataSource 적용
    리스트1-43 DataSource타입의 dataSource 빈 정의 메소드
    리스트1-44 DataSource타입의 빈을 DI받는 userDao() 빈 정의 메소드

--- chap01/src/main/java/springbook/user/dao/DaoFactory.java
+++ chap01/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,14 +1,17 @@
 package springbook.user.dao;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
 	@Bean
 	public UserDao userDao() {
 		UserDao dao = new UserDao();
-		dao.setConnectionMaker(connectionMaker());
+		dao.setConnectionMaker(dataSource());
 		return dao;
 	}
 	public AccountDao accountDao() {
@@ -19,7 +22,13 @@ public class DaoFactory {
 		return new MessageDao(connectionMaker());
 	}
 	@Bean
-	public ConnectionMaker connectionMaker() {
-		return new DConnectionMaker();
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+		dataSource.setDriverClass(org.h2.Driver.class);
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 }
