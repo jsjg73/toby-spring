d4395683f8333da417d1459481a69b9deb94a0e6
jsjg73 <kjs930703@naver.com>
2021-09-07 14:59:25 +0900

    2.3.3 포괄적인 테스트[1]

--- chap01/src/main/java/springbook/user/dao/UserDaoTest.java
+++ chap01/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -20,10 +20,7 @@ public class UserDaoTest {
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 		
-		User user = new User();
-		user.setId("jsjg73");
-		user.setName("재성");
-		user.setPassword("123");
+		User user = new User("jsjg73", "재성", "123");
 		
 		dao.add(user);
 		assertThat(dao.getCount(), is(1));
@@ -33,5 +30,25 @@ public class UserDaoTest {
 		assertThat(user2.getPassword(), is(user.getPassword()));
 
 	}
-	
+	@Test
+	public void count() throws SQLException{
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User("super", "길동이", "123");
+		User user2 = new User("son", "둘리", "234");
+		User user3 = new User("real", "희동이", "345");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+		
+		
+	}
 }
