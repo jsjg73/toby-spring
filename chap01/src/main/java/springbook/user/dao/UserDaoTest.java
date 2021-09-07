496813a847e8015220bbccd4a351a94b4f24e2a6
jsjg73 <kjs930703@naver.com>
2021-09-07 14:12:13 +0900

    2.2.2 검증 코드 전환 [2]
    리스트2-5 JUnit을 적용한 UserDaoTest

---
+++ chap01/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,36 @@
+package springbook.user.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.SQLException;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	
+	@Test
+	public void addAndGet() throws SQLException{
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("jsjg73");
+		user.setName("재성");
+		user.setPassword("123");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId()+" 등록 성공");
+		
+		User user2 = new User();
+		user2=dao.get("jsjg73");
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+
+	}
+	
+}
