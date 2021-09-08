27b5962b1d24b6111db1e86caa3f6af7e35c72a8
jsjg73 <kjs930703@naver.com>
2021-09-08 14:11:49 +0900

    진입지점 생성

--- chap01/src/main/java/springbook/user/main/EntryPoint.java
+++ chap01/src/main/java/springbook/user/main/EntryPoint.java
@@ -4,9 +4,8 @@ import java.sql.SQLException;
 
 import org.junit.runner.JUnitCore;
 
-/*리스트 1-3 테스트용 main() 메소드*/
 public class EntryPoint {
 	public static void main(String[] args) throws SQLException {
-		JUnitCore.main("springbook.test.UserDaoTest");
+		JUnitCore.main("springbook.user.dao.UserDaoTest");
 	}
 }
