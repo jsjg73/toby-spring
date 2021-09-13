15918e527dbb6dea7790c45744729952d4e47823
jsjg73 <kjs930703@naver.com>
2021-09-13 10:56:57 +0900

    3.3.2 전략과 클라이언트의 동거[4]
    로컬 클래스 활용
    
    리스트 3-20 익명 내부 클래스를 적용한 deleteAll() 메소드

--- chap01/src/main/java/springbook/user/dao/UserDao.java
+++ chap01/src/main/java/springbook/user/dao/UserDao.java
@@ -13,138 +13,135 @@ import springbook.user.domain.User;
 
 public class UserDao {
 	private DataSource dataSource; // 인터페이스를 통해 오브젝트에 접근하므로 구체적인 클래스 정보를 알 필요가 없다.
-	
+
 	public void setDataSource(DataSource dataSource) {
-		this.dataSource=dataSource;
+		this.dataSource = dataSource;
 	}
-	
+
 //	public UserDao(ConnectionMaker connectionMaker) {
 //		AnnotationConfigApplicationContext context =
 //				new AnnotationConfigApplicationContext(DaoFactory.class);
 //		this.connectionMaker=context.getBean("connectionMaker", ConnectionMaker.class);
 //	}
-	
+
 	/*
-	1.7.3 의존관계 검색과 주입
-	리스트 1-26 DaoFactory를 이용하는 생성자
-	public UserDao(){
-		DaoFacotry daoFactory = new DaoFactory();
-		this.connectionMaker = daoFactory.connectionMaker();
-	}
+	 * 1.7.3 의존관계 검색과 주입 리스트 1-26 DaoFactory를 이용하는 생성자 public UserDao(){ DaoFacotry
+	 * daoFactory = new DaoFactory(); this.connectionMaker =
+	 * daoFactory.connectionMaker(); }
 	 */
 
 	/*
-	1.7.3 의존관계 검색과 주입
-	리스트 1-27 의존관계 검색을 이용하는 UserDao생성자
-	public UserDao(){
-		AnnotationConfigApplicationContext context =
-				new AnnotationConfigApplicationContext(DaoFactory.class);
-		this.connectionMaker=context.getBean("connectionMaker", ConnectionMaker.class);
-	}
+	 * 1.7.3 의존관계 검색과 주입 리스트 1-27 의존관계 검색을 이용하는 UserDao생성자 public UserDao(){
+	 * AnnotationConfigApplicationContext context = new
+	 * AnnotationConfigApplicationContext(DaoFactory.class);
+	 * this.connectionMaker=context.getBean("connectionMaker",
+	 * ConnectionMaker.class); }
 	 */
-	
-	
+
 	public void add(final User user) throws SQLException {
-		jdbcContextWithStatementStrategy(
-				new StatementStrategy() {
-					public PreparedStatement makePreparedStatment(Connection c) throws SQLException {
-						PreparedStatement ps = c.prepareStatement(
-								"insert into users(id, name, password) values(?,?,?)");
-						ps.setString(1, user.getId());
-						ps.setString(2, user.getName());
-						ps.setString(3, user.getPassword());
-						
-						return ps;
-					}
-				}
-			);
+		jdbcContextWithStatementStrategy(new StatementStrategy() {
+			public PreparedStatement makePreparedStatment(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+
+				return ps;
+			}
+		});
 	}
-	public User get(String id)throws SQLException{
+
+	public User get(String id) throws SQLException {
 		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement(
-				"select * from users where id = ?");
+
+		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
-		
+
 		ResultSet rs = ps.executeQuery();
-		User user =null;
-		if(rs.next()) {
+		User user = null;
+		if (rs.next()) {
 			user = new User();
 			user.setId(rs.getString("id"));
 			user.setName(rs.getString("name"));
 			user.setPassword(rs.getString("password"));
 		}
-		
+
 		rs.close();
 		ps.close();
 		c.close();
-		
-		if(user == null)throw new EmptyResultDataAccessException(1);
+
+		if (user == null)
+			throw new EmptyResultDataAccessException(1);
 		return user;
 	}
-	
+
 	public void deleteAll() throws SQLException {
-		StatementStrategy st = new DeleteAllStatement();
-		jdbcContextWithStatementStrategy(st);
+		jdbcContextWithStatementStrategy(
+			new StatementStrategy() {
+				public PreparedStatement makePreparedStatment(Connection c) throws SQLException {
+					PreparedStatement ps = c.prepareStatement("delete from users");
+					return ps;
+				}
+			}
+		);
 	}
-	
-	public int getCount() throws SQLException{
+
+	public int getCount() throws SQLException {
 		Connection c = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
 			c = dataSource.getConnection();
-			ps = c.prepareStatement(
-					"select count(*) from users");
-			
+			ps = c.prepareStatement("select count(*) from users");
+
 			rs = ps.executeQuery();
 			rs.next();
 			return rs.getInt(1);
-			
-		}catch (SQLException e) {
+
+		} catch (SQLException e) {
 			throw e;
-		}finally {
-			if(rs!=null) {
+		} finally {
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (SQLException e) {
 				}
 			}
-			if(ps!=null) {
+			if (ps != null) {
 				try {
 					ps.close();
 				} catch (SQLException e) {
 				}
 			}
-			if(c!=null) {
+			if (c != null) {
 				try {
 					c.close();
 				} catch (SQLException e) {
 				}
 			}
 		}
-		
+
 	}
-	
-	public void jdbcContextWithStatementStrategy(StatementStrategy stmt)throws SQLException{
+
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
 		Connection c = null;
 		PreparedStatement ps = null;
 		try {
-			c= dataSource.getConnection();
-			
+			c = dataSource.getConnection();
+
 			ps = stmt.makePreparedStatment(c);
-			
+
 			ps.executeUpdate();
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			throw e;
-		}finally {
-			if(ps!=null) {
+		} finally {
+			if (ps != null) {
 				try {
 					ps.close();
 				} catch (SQLException e) {
 				}
 			}
-			if(c!=null) {
+			if (c != null) {
 				try {
 					c.close();
 				} catch (SQLException e) {
