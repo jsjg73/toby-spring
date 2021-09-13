85d53e6f8c85dd4663ed545910372b50439541cf
jsjg73 <kjs930703@naver.com>
2021-09-13 10:49:12 +0900

    3.3.2 전략과 클라이언트의 동거
    로컬 클래스 활용
    
    리스트 3-17 add() 메소드의 로컬 변수를 직접 사용하도록 수정한 AddStatement

--- chap01/src/main/java/springbook/user/dao/UserDao.java
+++ chap01/src/main/java/springbook/user/dao/UserDao.java
@@ -44,8 +44,19 @@ public class UserDao {
 	 */
 	
 	
-	public void add(User user) throws SQLException {
-		StatementStrategy st = new AddStatement(user);
+	public void add(final User user) throws SQLException {
+		class AddStatement implements StatementStrategy{
+			public PreparedStatement makePreparedStatment(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement(
+						"insert into users(id, name, password) values(?,?,?)");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+				
+				return ps;
+			}
+		}
+		StatementStrategy st = new AddStatement();
 		jdbcContextWithStatementStrategy(st);
 	}
 	public User get(String id)throws SQLException{
