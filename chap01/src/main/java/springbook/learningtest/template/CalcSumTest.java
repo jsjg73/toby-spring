42078930d0b51e4076b575cc9d58f6c6b9bc08d7
jsjg73 <kjs930703@naver.com>
2021-09-14 11:20:55 +0900

    3.5.3 템플릿/콜백의 응용[3]
    리스트3-33 BufferedReader를 전달받는 콜백 인터페이스
    리스트3-34 BufferedReaderCallback을 사용하는 템플릿 메소드
    리스트3-35 템플릿/콜백을 적용한 calcSum()메소드
    리스트3-36 새로운 테스트 메소드르르 추가한 CalcSumTest
    리스트3-37 곱을 계산하는 콜백을 가진 calcMultiply() 메소드

--- chap01/src/main/java/springbook/learningtest/template/CalcSumTest.java
+++ chap01/src/main/java/springbook/learningtest/template/CalcSumTest.java
@@ -5,14 +5,25 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcSumTest {
+	private Calculator calculator;
+	private String numFilePath;
+	@Before
+	public void setUp() {
+		this.calculator=new Calculator();
+		this.numFilePath=getClass().getResource("numbers.txt").getPath();
+	}
 	@Test
 	public void sumOfNumbers() throws IOException {
-		Calculator calculator = new Calculator();
-		int sum = calculator.calcSum(getClass().getResource(
-				"numbers.txt").getPath());
+		int sum = calculator.calcSum(this.numFilePath);
 		assertThat(sum, is(10));
+	}
+	
+	@Test
+	public void multiplyOfNumbers()throws IOException{
+		assertThat(calculator.calcMultiply(this.numFilePath), is(24));
 	}
 }
