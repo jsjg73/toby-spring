447921e2c19363f7fdf6504840aa6c8b87f18f12
jsjg73 <kjs930703@naver.com>
2021-09-16 11:39:13 +0900

    3.6.5 재사용 가능한 콜백의 분리 [1]
    리스트3-55 불필요한 DataSource 변수를 제거하고 남은 UserDao의 DI 코드
    리스트3-56 재사용 가능하도록 독립시킨 RowMapper
    리스트3-57 공유 userMapper를 사용하도록 수정한 get(), getAll()

--- chap01/src/main/java/springbook/user/dao/UserDao.java
+++ chap01/src/main/java/springbook/user/dao/UserDao.java
@@ -3,21 +3,27 @@ package springbook.user.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
 import javax.sql.DataSource;
-
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
 public class UserDao {
-	private DataSource dataSource; // 인터페이스를 통해 오브젝트에 접근하므로 구체적인 클래스 정보를 알 필요가 없다.
+	// 리스트 3-56 재사용 가능하도록 독립시킨 RowMapper
+	private RowMapper<User> userMapper = new RowMapper<User>() {
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		}
+	};
 	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
-		this.dataSource = dataSource;
 	}
 
 	public void add(final User user) {
@@ -27,16 +33,7 @@ public class UserDao {
 
 	public User get(String id) {
 		return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-				new Object[] {id},
-				new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-				});
+				new Object[] {id}, this.userMapper);
 	}
 
 	public void deleteAll() {
@@ -44,28 +41,9 @@ public class UserDao {
 	}
 
 	public int getCount() {
-//		return this.jdbcTemplate.query(new PreparedStatementCreator() {
-//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//				return con.prepareStatement("select count(*) from users");
-//			}
-//		}, new ResultSetExtractor<Integer>() {
-//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-//				rs.next();
-//				return rs.getInt(1);
-//			}
-//		});
 		return this.jdbcTemplate.queryForInt("select count(*) from users");
 	}
 	public List<User> getAll(){
-		return this.jdbcTemplate.query("select * from users order by id", 
-				new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-				});
+		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
 	}
 }
