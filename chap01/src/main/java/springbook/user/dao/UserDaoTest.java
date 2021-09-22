8478541444c8dce8db26c840869db4380d920cc3
jsjg73 <kjs930703@naver.com>
2021-09-22 17:13:41 +0900

    4.2.4 기술에 독립적인 UserDao[1]
    리스트4-20 UserDao 인터페이스
    리스트4-21 빈 클래스 변경
    리스트4-22 DataAccessException에 대한 테스트

--- chap01/src/main/java/springbook/user/dao/UserDaoTest.java
+++ chap01/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -10,12 +10,21 @@ import javax.sql.DataSource;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.domain.User;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
+	@Autowired
 	private UserDao dao;
 	private User user1;
 	private User user2;
@@ -26,10 +35,6 @@ public class UserDaoTest {
 		user1 = new User("jsjg73", "재성", "123");
 		user2 = new User("kjs9373", "김재성", "123");
 		user3 = new User("real", "희동이", "345");
-		dao = new UserDao();
-		DataSource dataSource = new SingleConnectionDataSource(
-				"jdbc:h2:tcp://localhost/~/test","sa","",true);
-		dao.setDataSource(dataSource);
 	}
 	@Test
 	public void addAndGet() throws SQLException{
@@ -111,5 +116,13 @@ public class UserDaoTest {
 		assertThat(user1.getId(), is(user2.getId()));
 		assertThat(user1.getName(), is(user2.getName()));
 		assertThat(user1.getPassword(), is(user2.getPassword()));
+	}
+	
+	@Test(expected = DuplicateKeyException.class)
+	public void duplicateKey() {
+		dao.deleteAll();
+
+		dao.add(user1);
+		dao.add(user1);
 	}
 }
