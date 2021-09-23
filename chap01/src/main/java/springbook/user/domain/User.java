bf912c6ab095ffe5d2d7c4e4331d94a6ab1b2547
jsjg73 <kjs930703@naver.com>
2021-09-23 16:45:44 +0900

    5.1.1 [2] User에 필드 추가
    리스트5-4 User에 추가된 필드

--- chap01/src/main/java/springbook/user/domain/User.java
+++ chap01/src/main/java/springbook/user/domain/User.java
@@ -4,7 +4,34 @@ public class User {
 	String id;
 	String name;
 	String password;
+	Level level;
+	int login;
+	int recommend;
 	
+	public Level getLevel() {
+		return level;
+	}
+
+	public void setLevel(Level level) {
+		this.level = level;
+	}
+
+	public int getLogin() {
+		return login;
+	}
+
+	public void setLogin(int login) {
+		this.login = login;
+	}
+
+	public int getRecommend() {
+		return recommend;
+	}
+
+	public void setRecommend(int recommend) {
+		this.recommend = recommend;
+	}
+
 	public User() {}
 	
 	public User(String id, String name, String password) {
