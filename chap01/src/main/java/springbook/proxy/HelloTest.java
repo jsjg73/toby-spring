647d0d1718fac3f5ca3e3b42cbb1e303509f7215
jsjg73 <kjs930703@naver.com>
2021-10-04 10:09:40 +0900

    6.3.2 [4] InvocationHandler and Proxy.newProxyInstance
    리스트6-23 InvocationHandler 구현 클래스
    리스트6-24 프록시 생성

--- chap01/src/main/java/springbook/proxy/HelloTest.java
+++ chap01/src/main/java/springbook/proxy/HelloTest.java
@@ -3,6 +3,8 @@ package springbook.proxy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.lang.reflect.Proxy;
+
 import org.junit.Test;
 
 public class HelloTest {
@@ -17,5 +19,13 @@ public class HelloTest {
 		assertThat(proxyHello.sayHello("jsjg73"), is("HELLO JSJG73"));
 		assertThat(proxyHello.sayHi("jsjg73"), is("HI JSJG73"));
 		assertThat(proxyHello.sayThankYou("jsjg73"), is("THANK YOU JSJG73"));
+		
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] {Hello.class}, 
+				new UppercaseHandler(new HelloTarget()));
+		assertThat(proxiedHello.sayHello("jsjg73"), is("HELLO JSJG73"));
+		assertThat(proxiedHello.sayHi("jsjg73"), is("HI JSJG73"));
+		assertThat(proxiedHello.sayThankYou("jsjg73"), is("THANK YOU JSJG73"));
 	}
 }
