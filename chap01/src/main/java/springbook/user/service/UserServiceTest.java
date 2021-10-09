363953a4e60dcc88c8604dbcc1a3268e474d5e3f
jsjg73 <kjs930703@naver.com>
2021-10-09 18:01:54 +0900

    6.6.4 [2] 트랜잭션 속성 적용 테스트-H2 readOnly불가
    리스트6-81 읽기전용 메소드에 쓰기작업을 추가한 테스트용 클래스
    리스트6-82 읽기전용 속성 테스트

--- chap01/src/main/java/springbook/user/service/UserServiceTest.java
+++ chap01/src/main/java/springbook/user/service/UserServiceTest.java
@@ -123,11 +123,19 @@ public class UserServiceTest {
 		
 		checkLevel(users.get(1), false);
 	}
-	static class TestUserServiceImpl extends UserServiceImpl{
+	static class TestUserService extends UserServiceImpl{
 		private String id ="Ron";
 		protected void upgradeLevel(User user) {
 			if(user.getId().equals(this.id)) throw new TestUserServiceException();
 			super.upgradeLevel(user);
+		}
+		
+		@Override
+		public List<User> getAll() {
+			for(User user : super.getAll()) {
+				super.update(user);
+			}
+			return null;
 		}
 	}
 	static class TestUserServiceException extends RuntimeException{}
@@ -204,5 +212,10 @@ public class UserServiceTest {
 		List<SimpleMailMessage> mailMessages = mailMessageArg.getAllValues();
 		assertThat(mailMessages.get(0).getTo()[0], is(users.get(1).getEmail()));
 		assertThat(mailMessages.get(1).getTo()[0], is(users.get(3).getEmail()));
+	}
+	
+	@Test
+	public void readOnlyTransactionAtrribute() {
+		testUserService.getAll();
 	}
 }
