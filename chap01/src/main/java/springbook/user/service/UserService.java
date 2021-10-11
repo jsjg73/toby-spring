b7398016e5e16e45dfd4e9de7061a0859b6e077e
jsjg73 <kjs930703@naver.com>
2021-10-12 08:21:22 +0900

    6.7.2 [1] 트랜잭션 어노테이션 적용 @Transactional
    리스트6-87 @Transactional 애노테이션을 이용한 속성 부여

--- chap01/src/main/java/springbook/user/service/UserService.java
+++ chap01/src/main/java/springbook/user/service/UserService.java
@@ -2,15 +2,22 @@ package springbook.user.service;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import springbook.user.domain.User;
 
+@Transactional
 public interface UserService {
 
 	public void upgradeLevels();
-	public User get(String id) ;
-	public List<User> getAll();
 	public void deleteAll() ;
 	public void update(User user);
 	public void add(User user);
+	
+	@Transactional(readOnly = true)
+	public User get(String id) ;
+	
+	@Transactional(readOnly = true)
+	public List<User> getAll();
 
 }
