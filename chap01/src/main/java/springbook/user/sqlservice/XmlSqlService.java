651b481347274414ddebcabf5918ff8742fc52aa
jsjg73 <kjs930703@naver.com>
2021-10-18 11:29:34 +0900

    7.2.5 [3] XmlSqlService 자기참조 - SqlReader
    7-34 SqlReader 구현
    XmlSqlService 내의 다른 변수와 메소드를 직접 참조하거나 사용하면 안 된다.
    필요한 경우에만 적절한 인터페이스를 통해 접근하도록 한다.

--- chap01/src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ chap01/src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -12,7 +12,7 @@ import springbook.user.dao.UserDao;
 import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
-public class XmlSqlService implements SqlService, SqlRegistry {
+public class XmlSqlService implements SqlService, SqlRegistry, SqlReader {
 	Map<String, String> sqlMap = new HashMap<String, String>();
 	private String sqlmapFile;
 	private SqlRegistry sqlRegistry;
@@ -69,5 +69,24 @@ public class XmlSqlService implements SqlService, SqlRegistry {
 			throw new SqlNotFoundException(key+
 					"에 대한 SQL을 찾을 수 없습니다.");
 		return sql;
+	}
+	
+	@Override
+	public void read(SqlRegistry sqlRegistry) {
+		String contextPath = Sqlmap.class.getPackage().getName();
+		try {
+			
+			JAXBContext context =  JAXBContext.newInstance(contextPath);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			
+			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(UserDao.class.getResourceAsStream(this.sqlmapFile));
+			
+			for(SqlType sql : sqlmap.getSql()) {
+				sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+			}
+		}catch(JAXBException e) {
+			throw new RuntimeException(e);
+		}
+		
 	}
 }
