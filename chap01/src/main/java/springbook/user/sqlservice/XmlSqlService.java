49a5b4d95b0611f301a7cd7d51a3c2398da41376
jsjg73 <kjs930703@naver.com>
2021-10-18 11:25:46 +0900

    7.2.5 [2] XmlSqlService 자기참조 - SqlRegistry
    7-33 SqlRegistry 구현

--- chap01/src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ chap01/src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -12,7 +12,7 @@ import springbook.user.dao.UserDao;
 import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
-public class XmlSqlService implements SqlService {
+public class XmlSqlService implements SqlService, SqlRegistry {
 	Map<String, String> sqlMap = new HashMap<String, String>();
 	private String sqlmapFile;
 	private SqlRegistry sqlRegistry;
@@ -47,11 +47,26 @@ public class XmlSqlService implements SqlService {
 			throw new RuntimeException(e);
 		}
 	}
-
+	
+	@Override
 	public String getSql(String key) throws SqlRetrievalFailureException {
 		String sql = sqlMap.get(key);
 		if(sql == null)
 			throw new SqlRetrievalFailureException(key+
+					"에 대한 SQL을 찾을 수 없습니다.");
+		return sql;
+	}
+
+	@Override
+	public void registerSql(String key, String sql) {
+		sqlMap.put(key, sql);
+	}
+
+	@Override
+	public String findsql(String key) throws SqlNotFoundException {
+		String sql = sqlMap.get(key);
+		if(sql == null)
+			throw new SqlNotFoundException(key+
 					"에 대한 SQL을 찾을 수 없습니다.");
 		return sql;
 	}
