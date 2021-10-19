1b15b177131f4bf4f5997ef1d65a6bd16f190ed2
jsjg73 <kjs930703@naver.com>
2021-10-20 08:14:38 +0900

    7.3.2 [2] OxmSqlService 내부 오브젝트로 프로퍼티 전달
    7-51 내부 오브젝트의 프로퍼티를 전달해주는 코드

--- chap01/src/main/java/springbook/user/sqlservice/OxmSqlService.java
+++ chap01/src/main/java/springbook/user/sqlservice/OxmSqlService.java
@@ -5,6 +5,13 @@ import org.springframework.oxm.Unmarshaller;
 public class OxmSqlService implements SqlService {
 	private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
 	
+	public void setUnmarshaller(Unmarshaller unmarshaller) {
+		this.oxmSqlReader.setUnmarshaller(unmarshaller);
+	}
+	
+	public void setSqlmapFile(String sqlmapFile) {
+		this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+	}
 	@Override
 	public String getSql(String key) throws SqlRetrievalFailureException {
 		// TODO Auto-generated method stub
@@ -14,8 +21,15 @@ public class OxmSqlService implements SqlService {
 	private class OxmSqlReader implements SqlReader{
 		private Unmarshaller unmarshaller;
 		
+		private final static String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
+		private String sqlmapFile = DEFAULT_SQLMAP_FILE;
+		
 		public void setUnmarshaller(Unmarshaller unmarshaller) {
 			this.unmarshaller = unmarshaller;
+		}
+
+		public void setSqlmapFile(String sqlmapFile) {
+			this.sqlmapFile = sqlmapFile;
 		}
 
 		@Override
