c6ebb0ef0d19cb47a90de0d230f0776c1ee36a46
jsjg73 <kjs930703@naver.com>
2021-10-22 20:50:34 +0900

    7.5.2 [3] 내장형 DB로 SqlRegistry 만들기
    7-71 HSQL 내장형 DB 설정
    7-72 내장형 DB를 사용하는 SQL레지스트리 - EmbeddedDbSqlRegistry

---
+++ chap01/src/main/java/springbook/issutracker/sqlservice/EmbeddedDbSqlRegistry.java
@@ -0,0 +1,48 @@
+package springbook.issutracker.sqlservice;
+
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+
+import springbook.user.sqlservice.SqlNotFoundException;
+
+public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
+	SimpleJdbcTemplate jdbc;
+
+	public void setDataSource(DataSource dataSource) {
+		jdbc = new SimpleJdbcTemplate(dataSource);
+	}
+	
+	@Override
+	public void registerSql(String key, String sql) {
+		jdbc.update("insert into sqlmap(key_, sql_) values(?,?)", key, sql);
+	}
+
+	@Override
+	public String findsql(String key) throws SqlNotFoundException {
+		try {
+			return jdbc.queryForObject("select sql_ from sqlmap where key_ = ?", String.class, key);
+		}catch(EmptyResultDataAccessException e) {
+			throw new SqlNotFoundException(key + "에 대한 SQL을 찾을 수 없습니다.", e);
+		}
+	}
+
+	@Override
+	public void updateSql(String key, String sql) throws SqlUpdateFailureException {
+		int affected = jdbc.update("update sqlmap set sql_ = ? where key_=?", sql, key);
+		if(affected==0) {
+			throw new SqlUpdateFailureException(key + "에 대한 SQL을 찾을 수 없습니다.");
+		}
+	}
+
+	@Override
+	public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
+		for(Map.Entry<String, String> entry: sqlmap.entrySet()) {
+			updateSql(entry.getKey(), entry.getValue());
+		}
+	}
+
+}
