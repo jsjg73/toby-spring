0f92517e7a0889da961c9becee1139613656c335
jsjg73 <kjs930703@naver.com>
2021-10-25 22:48:48 +0900

    7.5.3 [2] TransactionTemplate  트랜잭션 적용
    7-81 트랜잭션 기능을 가진 EmbeddedDbSqlRegistry

--- chap01/src/main/java/springbook/issutracker/sqlservice/EmbeddedDbSqlRegistry.java
+++ chap01/src/main/java/springbook/issutracker/sqlservice/EmbeddedDbSqlRegistry.java
@@ -6,14 +6,20 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import springbook.user.sqlservice.SqlNotFoundException;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 	SimpleJdbcTemplate jdbc;
+	TransactionTemplate transactionTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		jdbc = new SimpleJdbcTemplate(dataSource);
+		transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
 	}
 	
 	@Override
@@ -39,10 +45,15 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 	}
 
 	@Override
-	public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-		for(Map.Entry<String, String> entry: sqlmap.entrySet()) {
-			updateSql(entry.getKey(), entry.getValue());
-		}
+	public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException {
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				for(Map.Entry<String, String> entry: sqlmap.entrySet()) {
+					updateSql(entry.getKey(), entry.getValue());
+				}
+			}
+		});
 	}
 
 }
--- chap01/src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ chap01/src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -5,11 +5,16 @@ import static org.junit.Assert.fail;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import org.junit.After;
 import org.junit.Test;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import springbook.issutracker.sqlservice.AbstractUpdatableSqlRegistryTest;
 import springbook.issutracker.sqlservice.EmbeddedDbSqlRegistry;
@@ -18,6 +23,7 @@ import springbook.issutracker.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
 	EmbeddedDatabase db;
+	
 	@Override
 	protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
 		db = new EmbeddedDatabaseBuilder()
