d585891ad293972e9d6c6d93a67ecb1e4d7e4106
jsjg73 <kjs930703@naver.com>
2021-10-26 23:15:04 +0900

    7.6.1 [3] TestApplicationContext에 XML 파일 등록 - 점진적 개선
    기존 context.xml 파일 정돈
    환경 설정 클래스에

--- chap01/src/main/java/springbook/user/configuration/TestApplicationContext.java
+++ chap01/src/main/java/springbook/user/configuration/TestApplicationContext.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("/t-applicationContext.xml")
+@ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
 
 }
