fc98b0e6f24c6e6c2b9e18ef9c14e01320dbda25
jsjg73 <kjs930703@naver.com>
2021-10-27 09:48:44 +0900

    7.6.1 [4] dataSoucr 빈 전환
    7-88 자바 코드로 dataSource 빈 작성
    test-applicationContext.xml에서 빈 제거

--- chap01/src/main/java/springbook/user/configuration/TestApplicationContext.java
+++ chap01/src/main/java/springbook/user/configuration/TestApplicationContext.java
@@ -1,10 +1,27 @@
 package springbook.user.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import com.mysql.jdbc.Driver;
 
 @Configuration
 @ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
-
+	
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/spring_ex?characterEncoding=UTF-8");
+		dataSource.setUsername("jsjg73");
+		dataSource.setPassword("jsjg73");
+		
+		return dataSource;
+	}
 }
