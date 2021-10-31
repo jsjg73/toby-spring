54089ea142b5b2f6b55a4b6853c2c46de38253ef
jsjg73 <kjs930703@naver.com>
2021-10-31 23:45:14 +0900

    7.6.5 [1] @PropertySource와 @Value 적용, 그리고 database.properties 생성
    7-122 database.properties 파일
    7-123 @PropertySource 적용 - AppContext 클래스
    7-125 @Value("${치환자}")를 이용한 프로퍼티 값 주입
    7-126 프로퍼티 소스를 이용한 치환자 설정용 빈 - PropertySourcesPlaceholderconfigurer

--- chap01/src/main/java/springbook/user/configuration/AppContext.java
+++ chap01/src/main/java/springbook/user/configuration/AppContext.java
@@ -2,55 +2,48 @@ package springbook.user.configuration;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.mysql.jdbc.Driver;
 
-import springbook.issutracker.sqlservice.EmbeddedDbSqlRegistry;
-import springbook.user.dao.UserDao;
-import springbook.user.dao.UserDaoJdbc;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
-import springbook.user.service.UserServiceImpl;
 import springbook.user.service.UserServiceTest.TestUserService;
-import springbook.user.sqlservice.OxmSqlService;
-import springbook.user.sqlservice.SqlRegistry;
-import springbook.user.sqlservice.SqlService;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "springbook.user")
 @Import(SqlServiceContext.class)
+@PropertySource("database.properties")
 public class AppContext {
-	@Autowired UserDao userDao;
-	
 	/*
 	 * DB 연결과 트랜잭션
 	 * */
+	@Value("${db.driverClass}") Class<? extends Driver> driverClass;
+	@Value("${db.url}") String url;
+	@Value("${db.username}") String username;
+	@Value("${db.password}") String password;
+	
 	@Bean
 	public DataSource dataSource() {
 		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-		dataSource.setDriverClass(Driver.class);
-		dataSource.setUrl("jdbc:mysql://localhost/spring_ex?characterEncoding=UTF-8");
-		dataSource.setUsername("jsjg73");
-		dataSource.setPassword("jsjg73");
+		dataSource.setUrl(url);
+		dataSource.setDriverClass(driverClass);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
 
 		return dataSource;
 	}
@@ -61,6 +54,12 @@ public class AppContext {
 		tm.setDataSource(dataSource());
 		return tm;
 	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
 	@Configuration
 	@Profile("test")
 	public static class TestAppContext {
@@ -85,4 +84,6 @@ public class AppContext {
 			return mailSender;
 		}
 	}
+	
+	
 }
