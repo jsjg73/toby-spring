0e88f922e68af55f687ca878754f194264e39bc1
jsjg73 <kjs930703@naver.com>
2021-11-01 17:42:38 +0900

    7.6.6 [2] AppContext가 SqlMapConfig 구현
    7-134 SqlMapConfig을 구현하게 만든 AppContext
    UserSqlMapConfig 제거

--- chap01/src/main/java/springbook/user/configuration/AppContext.java
+++ chap01/src/main/java/springbook/user/configuration/AppContext.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -19,6 +21,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.mysql.jdbc.Driver;
 
+import springbook.user.dao.UserDao;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
 import springbook.user.service.UserServiceTest.TestUserService;
@@ -28,7 +31,7 @@ import springbook.user.service.UserServiceTest.TestUserService;
 @ComponentScan(basePackages = "springbook.user")
 @Import(SqlServiceContext.class)
 @PropertySource("database.properties")
-public class AppContext {
+public class AppContext implements SqlMapConfig {
 	/*
 	 * DB 연결과 트랜잭션
 	 * */
@@ -85,8 +88,8 @@ public class AppContext {
 		}
 	}
 	
-	@Bean
-	public SqlMapConfig sqlMapConfig() {
-		return new UserSqlMapConfig();
+	@Override
+	public Resource getSqlMapResource() {
+		return new ClassPathResource("sqlmap.xml", UserDao.class);
 	}
 }
